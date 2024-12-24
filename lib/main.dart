04908619fa5cecb95ffe1e321ebfc50c43dc81aa
Chamod Weerasinghe <chamodweerasinghe8@gmail.com>
2024-12-25 00:07:12 +0530

    add login_signup.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'login_signup.dart'; // Importing the new login_signup.dart file.
 
 void main() {
   runApp(const MyApp());
@@ -7,37 +8,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Firebase Auth Demo'),
+      home: const LoginSignupScreen(), // Set LoginSignupScreen as the initial screen.
     );
   }
 }
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-
-  const MyHomePage({super.key, required this.title});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: const Center(
-        child: Text('Test'),
-      ),
-    );
-  }
-}
-
-
